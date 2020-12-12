9be896776abf412b25320ed9382bcae78688c38a
PabloHdzVizcarra <GitHub3smuybu3n0_10>
2020-12-12 15:35:16 -0600

    Fix: test

--- src/routers/users/usersRouter.ts
+++ src/routers/users/usersRouter.ts
@@ -1,6 +1,6 @@
 import express, { Router } from 'express'
-import { createUser } from '../controllers/users/createUser'
-import { userValidationRules, validate } from '../models/validator/validator'
+import { createUser } from '../../controllers/users/createUser'
+import { userValidationRules, validate } from '../../models/validator/validator'
 const router: Router = express.Router()
 
 router.post('/', userValidationRules(), validate, createUser)

bff0c489458320dc1d32bf42222eafb65ea4f3a5
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2020-12-13 12:28:15 -0600

    Feat: new route "api/user/login" and added controller for this route

--- src/routers/users/usersRouter.ts
+++ src/routers/users/usersRouter.ts
@@ -1,12 +1,16 @@
 import express, { Request, Response, Router } from 'express'
 import { createUser } from '../../controllers/users/createUser'
-import { userValidationRules, validate } from '../../models/validator/validator'
+import {
+  loginValidationRules,
+  userValidationRules,
+  validate,
+} from '../../models/validator/validator'
 import { loginUser } from '../../controllers/users/loginUser'
 import { authenticateToken } from '../../controllers/users/auth/authenticateToken'
 const router: Router = express.Router()
 
 router.post('/', userValidationRules(), validate, createUser)
-router.post('/login', loginUser)
+router.post('/login', loginValidationRules(), validate, loginUser)
 router.get('/autologin', authenticateToken, (req: Request, res: Response) => {
   res.json({ validate: 'user' })
 })

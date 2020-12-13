70b854e19b04f3ed10973e27eeffaafa2474cb84
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2020-12-12 23:01:57 -0600

    Feat: new route POST "api/user/login"

--- src/routers/users/usersRouter.ts
+++ src/routers/users/usersRouter.ts
@@ -1,8 +1,10 @@
 import express, { Router } from 'express'
 import { createUser } from '../../controllers/users/createUser'
 import { userValidationRules, validate } from '../../models/validator/validator'
+import { loginUser } from '../../controllers/users/loginUser'
 const router: Router = express.Router()
 
 router.post('/', userValidationRules(), validate, createUser)
+router.post('/login', loginUser)
 
 export default router

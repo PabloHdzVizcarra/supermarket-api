fe351c6722c19076b422e2166c5e831827c73698
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2020-12-14 13:10:25 -0600

    Refactor: some files

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@ import './modules/mongoose/mongoose'
 import articlesRouter from './routers/articles/articlesRouter'
 import usersRouter from './routers/users/usersRouter'
 // import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 const app: Express = express()
 app.use(express.json())
+app.use(cookieParser())
 app.use('/api/article', articlesRouter)
 app.use('/api/user', usersRouter)
 
--- src/modules/debug-logs/debug.ts
+++ src/modules/debug-logs/debug.ts
@@ -3,3 +3,4 @@ import debug, { Debugger } from 'debug'
 export const LogRoute: Debugger = debug('info:route')
 export const LogError = debug('info:error')
 export const LogDatabase = debug('info:database')
+export const LogInfo = debug('info:')

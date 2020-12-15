640f557e8244b1b44205901be53a9b9298df25af
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2020-12-15 09:27:39 -0600

    Test: added route "api/user/autologin"

--- src/routers/users/__test__/router.authUser.test.ts
+++ src/routers/users/__test__/router.authUser.test.ts
@@ -1,15 +1,50 @@
 import app from '../../../index'
 import request from 'supertest'
-import * as helper from '../../../controllers/users/helpers/authenticateToken'
-import { Request, Response } from 'express'
 
 describe('Test route "api/user/autologin"', () => {
-  test('it should respond with a status code  401, and an object with the message property if there is no token in the request', async () => {
-    const result = await request(app)
+  test('it should respond with a status code  401, and an object with the message property if there is no token in the request', done => {
+    request(app)
       .get('/api/user/autologin')
-      .set('Cookie', [
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRhdGExMEBleGFtcGxlLmNvbSIsInBhc3N3b3JkIjoiMTIzNDU2Nzg5IiwiaWF0IjoxNjA3OTY1ODk5LCJleHAiOjE2MDgwNTIyOTl9.Ec196SgbdPheSrL_zk8AtnNEWgSZxkXkBjPtBYTEA38',
-      ])
-    console.log(result.status)
+      .set('Accept', 'application/json')
+      .expect('Content-Type', /json/)
+      .expect(401)
+      .end((error, result) => {
+        if (error) return done(error)
+        expect(result.body).toHaveProperty('message')
+        done()
+      })
   })
+  it('shout respond with a status code 403 and a message property, when an invalid a token is sent by the request', done => {
+    request(app)
+      .get('/api/user/autologin')
+      .set('Accept', 'application/json')
+      .set('Cookie', ['tokenWrong'])
+      .expect('Content-Type', /json/)
+      .expect(403)
+      .end((error, result) => {
+        if (error) return done(error)
+        expect(result.body).toHaveProperty('message')
+        return done()
+      })
+  })
+  //
+  // it('should pass to following middleware when a correct token is sent in the request to the endpoint', done => {
+  //   const res = {} as Response
+  //   const req = {} as Request
+  //   const next = jest.fn()
+  //   res.status = jest.fn().mockReturnValue(res)
+  //   res.json = jest.fn().mockReturnValue(res)
+  //   jest
+  //     .spyOn(helper, 'authenticateToken')
+  //     .mockImplementation((req, res, next) => undefined)
+  //   request(app)
+  //     .get('/api/user/autologin')
+  //     .set('Accept', 'application/json')
+  //     .set('Cookie', ['correct-token'])
+  //     .end((error, response) => {
+  //       if (error) return done(error)
+  //       expect(response.status).toBe(400)
+  //       return done()
+  //     })
+  // })
 })

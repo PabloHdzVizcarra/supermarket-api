93e8a35491ec81237e67ba5f3f40c93e6e7159b3
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2020-12-16 11:25:05 -0600

    Test: finish test createUser controller

--- src/controllers/users/__test__/controller.createUser.test.ts
+++ src/controllers/users/__test__/controller.createUser.test.ts
@@ -1,25 +1,68 @@
-import request from 'supertest'
-import app from '../../../index'
-import * as func from '../helpers/createResponse'
+import { createUser } from '../createUser'
+import { Request, Response } from 'express'
+import * as mongoose from '../../../modules/mongoose/mongoose'
 
 describe('test in controller POST: "/api/user"', () => {
-  test('it should return a status code 400, and an error message, when an error occurs when saving the data in the database', async () => {
-    const mock = jest.spyOn(func, 'createResponse').mockResolvedValue({
-      statusCode: 400,
-      message: 'error save data',
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  const dataUser = {
+    name: 'John',
+    password: '123456789',
+    lastname: 'Smith',
+    username: 'john182',
+    email: 'example@data.com',
+    dateOfBirth: '1992/12/01',
+  }
+  const mockRequest = (body: { data: Record<string, string> }) =>
+    ({
+      body,
+    } as Request)
+  const mockResponse = () => {
+    const res = {} as Response
+    res.status = jest.fn().mockReturnValue(res)
+    res.json = jest.fn().mockReturnValue(res)
+    return res
+  }
+
+  it('should respond with a 204 status code, and an error message when all data in requested in the data request is not sent, data "name-username-password-lastname-email-dateOfBirth" ', async () => {
+    const wrongDataUser = {
+      name: 'John',
+      password: '123456789',
+      email: 'example@data.com',
+    }
+    const res = mockResponse()
+    const req = mockRequest({ data: wrongDataUser })
+    await createUser(req, res)
+    expect(res.status).toHaveBeenCalledWith(204)
+    expect(res.json).toHaveBeenCalledWith({ message: expect.any(String) })
+  })
+
+  it('should response a status code 400, and an error message when all the correct data is sent in te request, but an error occurs in the database when saving the data', async () => {
+    jest
+      .spyOn(mongoose, 'createDataInCollection')
+      .mockResolvedValue({ error: true, message: 'some database error' })
+    const res = mockResponse()
+    const req = mockRequest({ data: dataUser })
+    await createUser(req, res)
+    expect(res.status).toHaveBeenCalledWith(400)
+    expect(res.json).toHaveBeenCalledWith({ message: 'some database error' })
+  })
+
+  it('should respond with a status code 201 and an object with the message and data properties with the user data if all the data is correctly sent to the database and saved correctly', async () => {
+    jest.spyOn(mongoose, 'createDataInCollection').mockResolvedValue({
+      error: false,
+      message: 'document created',
+      data: dataUser as never,
     })
-    const result = await request(app).post('/api/user').send({
-      name: 'pablo',
-      lastname: 'viscera',
-      username: 'dev-text',
-      password: '12345678',
-      yearOfBirth: '1992/07/13',
-      email: 'textemail@google.com',
+    const res = mockResponse()
+    const req = mockRequest({ data: dataUser })
+    await createUser(req, res)
+    expect(res.status).toHaveBeenCalledWith(201)
+    expect(res.json).toHaveBeenCalledWith({
+      message: 'document created',
+      data: dataUser,
     })
-    expect(result.status).toBe(400)
-    expect(result.body).toEqual({
-      message: expect.any(String),
-    })
-    mock.mockRestore()
   })
 })

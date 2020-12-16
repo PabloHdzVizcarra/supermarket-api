e2d86c36ab727a3c918b766524f7eec533aaf2d9
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2020-12-16 11:26:57 -0600

    Feat: finish controller createUser controller

--- src/controllers/users/createUser.ts
+++ src/controllers/users/createUser.ts
@@ -5,13 +5,23 @@ import { createResponse } from './helpers/createResponse'
 import { LogRoute } from '../../modules/debug-logs/debug'
 import { hashPassword } from './helpers/hashPassword'
 import { UserModel } from '../../types/types'
+import { checkPropertiesInObject } from './helpers/checkProperties/checkPropertiesInObject'
 
 export async function createUser(req: Request, res: Response): Promise<void> {
   LogRoute('POST: /api/user')
-  const dataHash: UserModel = hashPassword(req.body)
+  const checked = checkPropertiesInObject(
+    ['name', 'username', 'password', 'lastname', 'email', 'dateOfBirth'],
+    req.body.data,
+  )
+  if (!checked) {
+    res.status(204).json({ message: 'empty fields were sent' })
+    return
+  }
+  const dataHash: UserModel = hashPassword(req.body.data)
   const { statusCode, message, data } = await createResponse(
     await createDataInCollection(dataHash, UserSchema),
   )
+
   res.status(statusCode).json({
     message,
     data,

3b9ae5b0dcaa022de230d165fe840a6e23b6680e
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2020-12-17 08:42:12 -0600

    Feat: added endpoint POST "api/article/" to create articles

--- src/routers/articles/articlesRouter.ts
+++ src/routers/articles/articlesRouter.ts
@@ -1,7 +1,11 @@
 import express, { Router } from 'express'
-import { createArticle } from '../../controllers/articles/articlesController'
+import { createArticle } from '../../controllers/articles/create-article/createArticle'
+import {
+  articleValidationRules,
+  validate,
+} from '../../models/validator/validator'
 const router: Router = express.Router()
 
-router.post('/', createArticle)
+router.post('/', articleValidationRules(), validate, createArticle)
 
 export default router

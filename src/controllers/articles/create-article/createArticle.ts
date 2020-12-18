6ec7febd02972a25a979b468bae7f3ee6fd4700d
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2020-12-18 07:53:38 -0600

    Refactor: controller

--- src/controllers/articles/create-article/createArticle.ts
+++ src/controllers/articles/create-article/createArticle.ts
@@ -2,23 +2,14 @@ import { Request, Response } from 'express'
 import { createOneDataInSchema } from '../../../modules/mongoose/actions-mongoose/createOneDataInSchema'
 import ArticleSchema from '../../../models/articlesModel/articlesModel'
 import { createResponse } from '../../users/helpers/createResponse'
-import { LogRoute } from '../../../modules/debug-logs/debug'
-import { embedDocument } from '../../../modules/mongoose/actions-mongoose/embedDocument'
-import UserSchema from '../../../models/userModel'
-import { createDataToRespond } from '../../../modules/mongoose/actions-mongoose/createRespond'
 
 export async function createArticle(
   req: Request,
   res: Response,
 ): Promise<void> {
-  LogRoute('POST /api/article/')
-  const { creator_user } = req.body
+  console.log(req.body)
   const { statusCode, message, data } = await createResponse(
     await createOneDataInSchema(req.body, ArticleSchema),
   )
-  const result = await createDataToRespond(
-    await embedDocument(req.body, creator_user, UserSchema as never),
-  )
-  console.log(result)
   res.status(statusCode).json({ message, data })
 }

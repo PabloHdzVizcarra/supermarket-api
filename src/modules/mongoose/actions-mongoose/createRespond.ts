1adbb61fe314c32dc213de41b1ef488b4c00d59f
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2020-12-18 07:55:58 -0600

    Feat: createRespond helper function

---
+++ src/modules/mongoose/actions-mongoose/createRespond.ts
@@ -0,0 +1,24 @@
+import { DataToResponse, ResultSavedWithError } from '../../../types/types'
+
+export async function createDataToRespond(
+  resultFromDatabase: ResultSavedWithError,
+): Promise<DataToResponse> {
+  const { error, databaseError, data, message } = resultFromDatabase
+  if (databaseError) {
+    return {
+      statusCode: 400,
+      message,
+    }
+  }
+  if (error) {
+    return {
+      statusCode: 404,
+      message,
+    }
+  }
+  return {
+    statusCode: 201,
+    message,
+    data,
+  }
+}

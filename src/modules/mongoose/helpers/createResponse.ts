cddf1db9d91387e77f8f30e2028f6308f0f28d27
PabloHdzVizcarra <pablourbanohdzvizcarra@gmail.com>
2020-12-19 12:49:25 -0600

    Feat: added createResponse helper
    a helper is created to help with mongoose

---
+++ src/modules/mongoose/helpers/createResponse.ts
@@ -0,0 +1,38 @@
+import { ResultActionInDoc } from '../actions-mongoose/deleteDocument'
+import { Document } from 'mongoose'
+
+type DataToResponse = {
+  code: number
+  message: string
+  document?: Document
+}
+
+export async function createResponse({
+  message,
+  error,
+  doc,
+}: ResultActionInDoc): Promise<DataToResponse> {
+  switch (error) {
+    case 'database': {
+      return {
+        code: 400,
+        message,
+      }
+    }
+
+    case 'not-found': {
+      return {
+        code: 404,
+        message,
+      }
+    }
+
+    case 'null': {
+      return {
+        code: 200,
+        message,
+        document: doc,
+      }
+    }
+  }
+}
